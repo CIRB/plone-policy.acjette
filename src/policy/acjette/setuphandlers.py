5c1cec16e19ec0fdd171577ce3a8bacd19500143
U1210 <ccorbesier@ubuntu.(none)>
2012-12-31 00:26:55 -0800

    Automate creation of elements required to install plonetheme.acjette

---
+++ src/policy/acjette/setuphandlers.py
@@ -0,0 +1,105 @@
+from zope import component
+from zope.interface import alsoProvides
+#from z3c.saconfig.interfaces import IEngineFactory
+#from z3c.saconfig import Session
+import transaction
+from Products.CMFCore.utils import getToolByName
+from Products.LinguaPlone.browser.setup import SetupView
+
+def setupLinguaFolders(site, logger):
+    sw = SetupView(site, site.REQUEST)
+
+    sw.folders = {}
+    pl = getToolByName(site, "portal_languages")
+    sw.languages = pl.getSupportedLanguages()
+    if len(sw.languages) == 1:
+        logger.error('Only one supported language configured.')
+    sw.defaultLanguage = pl.getDefaultLanguage()
+    available = pl.getAvailableLanguages()
+    for language in sw.languages:
+        info = available[language]
+        sw.setUpLanguage(language, info.get('native', info.get('name')))
+    
+    sw.linkTranslations()
+    sw.removePortalDefaultPage()
+    #if sw.previousDefaultPageId:
+    #    sw.resetDefaultPage()
+    sw.setupLanguageSwitcher()
+
+def setupACJette(context):
+    logger = context.getLogger("setupACJette")
+    logger.info('start setup ACJette')
+    if context.readDataFile('policy.acjette.txt') is None:
+        return
+    site = context.getSite()
+    portal_workflow = site.portal_workflow
+    setupLinguaFolders(site, logger)
+    
+    fr = site.fr
+
+    fr.invokeFactory(type_name='Folder',
+        id='commun',
+        title='Commun',
+        description='',
+        language='fr')
+    commun = fr.commun
+    portal_workflow.doActionFor(commun, 'publish')
+    commun.setExcludeFromNav(True)
+    commun.reindexObject()
+
+    fr.commun.invokeFactory(type_name='Document',
+        id='topnav',
+        title='Topnav',
+        description='',
+        language='fr')
+    topnav = fr.commun.topnav
+    portal_workflow.doActionFor(topnav, 'publish')
+    topnav.setExcludeFromNav(True)
+    topnav.reindexObject()
+
+    fr.commun.invokeFactory(type_name='Document',
+        id='slider',
+        title='Slider',
+        description='',
+        language='fr')
+    slider = fr.commun.slider
+    portal_workflow.doActionFor(slider, 'publish')
+    slider.setExcludeFromNav(True)
+    slider.reindexObject()
+
+    nl = site.nl
+
+    nl.invokeFactory(type_name='Folder',
+        id='common',
+        title='Common',
+        description='',
+        language='nl')
+    common = nl.common
+    portal_workflow.doActionFor(common, 'publish')
+    common.addTranslationReference(commun)
+    common.setExcludeFromNav(True)
+    common.reindexObject()
+
+    nl.common.invokeFactory(type_name='Document',
+        id='topnav',
+        title='Topnav',
+        description='',
+        language='nl')
+    topnavnl = nl.common.topnav
+    portal_workflow.doActionFor(topnavnl, 'publish')
+    topnavnl.addTranslationReference(topnav)
+    topnavnl.setExcludeFromNav(True)
+    topnavnl.reindexObject()
+
+    nl.common.invokeFactory(type_name='Document',
+        id='slider',
+        title='Slider',
+        description='',
+        language='nl')
+    slidernl = nl.common.slider
+    portal_workflow.doActionFor(slidernl, 'publish')
+    slidernl.addTranslationReference(slider)
+    slidernl.setExcludeFromNav(True)
+    slidernl.reindexObject()
+
+    logger.info('end setup ACJette')
